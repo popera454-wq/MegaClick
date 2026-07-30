9756f641e15587ff9b98cf991ba2a481dff1066c
popera454-wq <popera454@gmail.com>
2026-07-30 14:47:38 +0300

    עדכון לוגו 3

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <Image
-      src="/images/megaclick-logo.png"
-      alt="MegaClick Logo"
-      width={160}
-      height={50}
-      priority
-    />
+    <div className="flex items-center h-full">
+      <Image
+        src="/images/megaclick-logo.png"
+        alt="MegaClick Logo"
+        width={140}
+        height={45}
+        className="object-contain max-h-[45px] w-auto"
+        priority
+      />
+    </div>
   );
 }
