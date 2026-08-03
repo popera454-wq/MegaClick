5a1589522b1ceb83b62737b56f7044a4693bf678
popera454-wq <popera454@gmail.com>
2026-08-03 21:41:25 +0300

    עדכו הפס השקוף

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -97,13 +97,13 @@ min-h-[82px]
 rounded-[28px]
 
 border
-border-white/12
+border-white/15
 
-bg-black/20
+bg-[#050509]/20
 
 backdrop-blur-3xl
 
-shadow-[0_20px_60px_rgba(0,0,0,.25)]
+shadow-[0_20px_60px_rgba(0,0,0,.35)]
 
 items-center
 justify-between
